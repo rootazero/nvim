8e942c4e16c6d974cad6e014ef37d27cd68f7380
rootazero <124485722+rootazero@users.noreply.github.com>
2025-12-07 15:51:50 +0800

    Update Python host program paths for Neovim

--- init.lua
+++ init.lua
@@ -3,9 +3,9 @@ require("config.lazy")
 
 -- 通过uv设置python环境
 if vim.loop.os_uname().sysname == "Darwin" or vim.loop.os_uname().sysname == "Linux" then
-  vim.g.python_host_prog = "$HOME/.uv/nvim/bin/python"
-  vim.g.python3_host_prog = "$HOME/.uv/nvim/bin/python"
+  vim.g.python_host_prog = "$HOME/.uv/nvim/.venv/bin/python"
+  vim.g.python3_host_prog = "$HOME/.uv/nvim/.venv/bin/python"
 elseif vim.loop.os_uname().sysname == "Windows_NT" then
-  vim.g.python_host_prog = "%USERPROFILE%\\.uv\\nvim\\bin\\python"
-  vim.g.python3_host_prog = "%USERPROFILE%\\.uv\\nvim\\bin\\python"
+  vim.g.python_host_prog = "%USERPROFILE%\\.uv\\nvim\\.venv\\bin\\python"
+  vim.g.python3_host_prog = "%USERPROFILE%\\.uv\\nvim\\.venv\\bin\\python"
 end
